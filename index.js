d1cf8211bc37eb20bc58cd20e55f8fa7bc92278b
DESKTOP-SO57OLA\Le Tan Dat <letandat260199@gmail.com>
2021-02-22 18:21:50 +0700

    Save and Load data.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+let todoStorageArr = [];
 let add = document.getElementById("addTodo");
 add.onclick = function () {
     let inputElement = document.getElementById("thingstodo");
     createTodoItem(inputElement.value);
+    todoStorageArr.push(inputElement.value);
+    saveData();
     inputElement.value = "";
 };
+
+window.onload = function () {
+    loadData();
+    
+    for (let i = 0; i < todoStorageArr.length; i++) {
+        createTodoItem(todoStorageArr[i]);
+    }
+}
 
 let sub = document.getElementById("thingstodo");
 sub.addEventListener("keyup", function (event) {
@@ -14,10 +25,18 @@ sub.addEventListener("keyup", function (event) {
 });
 
 function onCheckboxClick(element) {
+
+    const content = element.parentNode.querySelector(".todosAdd").innerText;
+    for (let i = 0; i < todoStorageArr.length; i++) {
+        if (todoStorageArr[i] == content) {
+            todoStorageArr.splice(i, 1);
+            saveData();
+        }
+    }
     element.parentNode.classList.add("deleteLi");
     setTimeout(function () {
         deleteElement(element.parentNode);
-    }, 1500)
+    }, 300)
 }
 
 function deleteElement(element) {
@@ -27,5 +46,23 @@ function deleteElement(element) {
 
 function createTodoItem(content) {
     document.getElementById("listtodo").innerHTML +=
-        "<li><i class='fas fa-check' onclick='onCheckboxClick(this)'></i>" + "<input type='checkbox'/>" + content + "</li>";
+        "<li class='setPriority'><i class='fas fa-check' onclick='onCheckboxClick(this)'></i>" + "<span class='todosAdd'>" + content + "</span>" + "</li>";
+}
+
+function saveData() {
+    if (todoStorageArr.length == 0) {
+        localStorage.removeItem("todoStorage");
+    } else {
+        localStorage.setItem("todoStorage", todoStorageArr.join());
+    }
+}
+
+function loadData() {
+    const data = localStorage.getItem("todoStorage");
+    if (data == null) {
+        todoStorageArr = [];
+    } else {
+        todoStorageArr = data.split(',');
+
+    }
 }
