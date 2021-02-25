9beba2452b5f14880a334d598acba239fcfc92b2
DESKTOP-SO57OLA\Le Tan Dat <letandat260199@gmail.com>
2021-02-25 13:11:20 +0700

    Store priority

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ add.onclick = function () {
     } else {
         createTodoItem(inputElement.value);
         todoStorageArr.push(inputElement.value);
+        colorStorageArr.push("black");
         saveData();
         inputElement.value = "";
         setEventPriorityButtonClick();
@@ -13,10 +14,18 @@ add.onclick = function () {
 };
 
 let todoStorageArr = [];
+let colorStorageArr = [];
 window.onload = function () {
     loadData();
     for (let i = 0; i < todoStorageArr.length; i++) {
         createTodoItem(todoStorageArr[i]);
+        if (colorStorageArr[i] == "red") {
+            red[i].parentNode.children[1].classList.add("redIsHigh");
+        } else if (colorStorageArr[i] == "yellow") {
+            yellow[i].parentNode.children[1].classList.add("yellowIsMedium");
+        } else if (colorStorageArr[i] == "green") {
+            green[i].parentNode.children[1].classList.add("greenIsLow");
+        }
     }
     setEventPriorityButtonClick();
 }
@@ -34,6 +43,7 @@ function onCheckboxClick(element) {
     for (let i = 0; i < todoStorageArr.length; i++) {
         if (todoStorageArr[i] == content) {
             todoStorageArr.splice(i, 1);
+            colorStorageArr.splice(i, 1);
             saveData();
         }
     }
@@ -56,46 +66,52 @@ function createTodoItem(content) {
 function saveData() {
     if (todoStorageArr.length == 0) {
         localStorage.removeItem("todoStorage");
+        localStorage.removeItem("colorStorage");
     } else {
-        localStorage.setItem("todoStorage", todoStorageArr.join());
+        localStorage.setItem("todoStorage", JSON.stringify(todoStorageArr));
+        localStorage.setItem("colorStorage", JSON.stringify(colorStorageArr));
     }
 }
 
 function loadData() {
-    const data = localStorage.getItem("todoStorage");
-    if (data == null) {
-        todoStorageArr = [];
-    } else {
-        todoStorageArr = data.split(',');
-    }
+    const dataTodo = localStorage.getItem("todoStorage");
+    const dataColor = localStorage.getItem("colorStorage");
+    todoStorageArr = dataTodo == null ? [] : JSON.parse(dataTodo);
+    colorStorageArr = dataColor == null ? [] : JSON.parse(dataColor);
 }
 
+let green = document.getElementsByClassName("lowBtn");
+let yellow = document.getElementsByClassName("mediumBtn");
+let red = document.getElementsByClassName("highBtn");
 function setEventPriorityButtonClick() {
-    let red = document.getElementsByClassName("highBtn");
     for (let i = 0; i < red.length; i++) {
         red[i].onclick = function () {
             if (red[i].parentNode.children[1].classList.contains("yellowIsMedium") || red[i].parentNode.children[1].classList.contains("greenIsLow")) {
                 red[i].parentNode.children[1].classList.remove("yellowIsMedium", "greenIsLow");
             };
             red[i].parentNode.children[1].classList.add("redIsHigh");
+            colorStorageArr[i] = "red";
+            saveData();
         }
     };
-    let yellow = document.getElementsByClassName("mediumBtn");
     for (let j = 0; j < yellow.length; j++) {
         yellow[j].onclick = function () {
             if (yellow[j].parentNode.children[1].classList.contains("redIsHigh") || yellow[j].parentNode.children[1].classList.contains("greenIsLow")) {
                 yellow[j].parentNode.children[1].classList.remove("redIsHigh", "greenIsLow");
             };
             yellow[j].parentNode.children[1].classList.add("yellowIsMedium");
+            colorStorageArr[j] = "yellow";
+            saveData();
         }
     }
-    let green = document.getElementsByClassName("lowBtn");
     for (let o = 0; o < green.length; o++) {
         green[o].onclick = function () {
             if (green[o].parentNode.children[1].classList.contains("yellowIsMedium") || green[o].parentNode.children[1].classList.contains("redIsHigh")) {
                 green[o].parentNode.children[1].classList.remove("yellowIsMedium", "redIsHigh");
             };
             green[o].parentNode.children[1].classList.add("greenIsLow");
+            colorStorageArr[o] = "green";
+            saveData();
         }
     }
 }
